b69a84d6ba3aefd6c28eedfc9bd1af9dbb888afe
Sho4kas <augustas.brasiunas@gmail.com>
2023-09-13 15:23:13 +0300

    first commit

---
+++ arrays.js
@@ -0,0 +1,45 @@
+// // let numsArrays = [5, 500, -5, 57.5, 'du']
+
+// // console.log(numsArrays)
+// // console.log(numsArrays.length)
+
+// // console.log(numsArrays[0])
+// // console.log(numsArrays[1])
+// // console.log(numsArrays[2])
+// // console.log(numsArrays[3])
+// // console.log(numsArrays[4])
+
+// // let newArr = ['vienas', 'du', 'du su puse', 5, true, [1,2,5]];
+
+// // console.log(newArr[0])
+// // console.log(newArr[1])
+// // console.log(newArr[2])
+// // console.log(newArr[3])
+// // console.log(newArr[4])
+// // console.log(newArr[5])
+// // console.log(newArr[5][0])
+// // console.log(newArr[5][1])
+// // console.log(newArr[5][2])
+
+// // console.log(newArr[1])
+// // newArr[1] = 2
+
+// // newArr[newArr.length] = 444
+
+// // console.log(newArr)
+
+// let cities = ['Vilnius', 'Kaunas', 'Klaipėda', 'Šiauliai', 'Panevėžys'];
+// console.log(cities)
+
+// // let lastcity = cities.pop()
+// // console.log(lastcity)
+
+// // cities.pop()
+
+// let firstcity = cities.shift()
+
+// cities.shift()
+// console.log(firstcity)
+
+
+
